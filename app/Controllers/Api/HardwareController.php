c47f25439717d6d861acf3f390a2d00e5d76e9f9
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-17 01:17:58 -0600

    Add AAT and BTN management API

---
+++ app/Controllers/Api/HardwareController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use App\Controllers\BaseController;
+use App\Services\Performance\HardwareManagerService;
+
+class HardwareController extends BaseController
+{
+    private function service(): HardwareManagerService
+    {
+        return new HardwareManagerService();
+    }
+
+    public function aats()
+    {
+        return $this->response->setJSON($this->service()->listAats());
+    }
+
+    public function createAat()
+    {
+        $data = $this->service()->createAat($this->request->getJSON(true) ?? []);
+        return $this->response->setStatusCode($data['success'] ? 201 : 400)->setJSON($data);
+    }
+
+    public function assignAat($aatId)
+    {
+        $data = $this->service()->assignAat((int)$aatId, $this->request->getJSON(true) ?? []);
+        return $this->response->setStatusCode($data['success'] ? 200 : 400)->setJSON($data);
+    }
+
+    public function returnAat($aatId)
+    {
+        $data = $this->service()->returnAat((int)$aatId);
+        return $this->response->setStatusCode($data['success'] ? 200 : 400)->setJSON($data);
+    }
+
+    public function aatHistory($aatId)
+    {
+        return $this->response->setJSON($this->service()->aatHistory((int)$aatId));
+    }
+
+    public function btns()
+    {
+        return $this->response->setJSON($this->service()->listBtns());
+    }
+
+    public function saveBtn($id = null)
+    {
+        $data = $this->service()->saveBtn($id !== null ? (int)$id : null, $this->request->getJSON(true) ?? []);
+        return $this->response->setStatusCode($data['success'] ? ($id ? 200 : 201) : 400)->setJSON($data);
+    }
+
+    public function btnHealth($id)
+    {
+        $data = $this->service()->updateBtnHealth((int)$id, $this->request->getJSON(true) ?? []);
+        return $this->response->setStatusCode($data['success'] ? 200 : 400)->setJSON($data);
+    }
+}
